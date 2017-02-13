c55d153e88e80eee06745fea51eeb09916f4b3d8
Alexander Kutin <Alexander.Kutin@broadridge.com>
2017-02-13 11:24:31 +0300

    prebuilt

---
+++ Prebuilt/Context/ContextState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prebuilt.Context
+{
+    public class ContextState
+    {
+        public static readonly ContextState Instance = new ContextState();
+
+        public bool VoiceEnabled { get; set; }
+    }
+}
---
+++ Prebuilt/Context/Management.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prebuilt.Context
+{
+    public class Management : PrebuiltBase
+    {
+        public Management(EventHandler<string> onMessage, string commandText) : base(onMessage, commandText)
+        {
+        }
+
+        public void VoiceOn()
+        {
+            OnMessage(this, "Voice listening On");
+            ContextState.Instance.VoiceEnabled = true;
+        }
+
+        public void VoiceOff()
+        {
+            OnMessage(this, "Voice listening Off");
+            ContextState.Instance.VoiceEnabled = false;
+        }
+    }
+}
